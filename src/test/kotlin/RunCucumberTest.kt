638b26cf64a78d8299b41c686ce46e8f30713fce
Hakanai <hakanai@ephemeral.garden>
2021-08-23 12:32:46 +1000

    Cucumber update to v6.10.4

--- src/test/kotlin/RunCucumberTest.kt
+++ src/test/kotlin/RunCucumberTest.kt
@@ -8,5 +8,5 @@ import org.junit.runner.RunWith
 @CucumberOptions(
         strict = true,
         features = ["src/features"],
-        tags = ["not @ignored"])
+        tags = "not @ignored")
 class RunCucumberTest
